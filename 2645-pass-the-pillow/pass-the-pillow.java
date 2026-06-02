20698f8d85afb11caa0554f8bbf0ec1942335118
Ayush sharma <ayushs9378@gmail.com>
2026-06-02 10:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (14.91%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+
+        int a = time / (n - 1);
+        int b = time % (n - 1);
+
+        if (a % 2 == 0) {
+            return b + 1;
+        }
+
+        return n - b;
+    }
+}
