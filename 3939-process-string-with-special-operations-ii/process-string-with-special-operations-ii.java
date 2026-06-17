a7326af23f072d0893032443c5a4743260ab675e
Ayush sharma <ayushs9378@gmail.com>
2026-06-17 16:29:50 +0530

    Time: 29 ms (73.08%) | Memory: 47.9 MB (42.31%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -12,7 +12,7 @@ class Solution {
             }
         }
         if (k >= m) return '.';
-        
+
         for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
             if (c == '*') {
